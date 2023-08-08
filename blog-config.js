3bf6960aaf88b4c237bb09fc9f0fb2eecf0905c8
caste77an <hancastellan@naver.com>
2023-08-08 23:52:59 +0900

    first commit

---
+++ blog-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "hoodie",
+  description:
+    "Hello :) I'm galaxymonster who developed gatsby-starter-hoodie theme.",
+  author: "Hudi",
+  siteUrl: "https://galaxymonster.github.io/",
+  links: {
+    github: "https://github.com/devHudi",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "mailto:hancastellan@gmail.com",
+  },
+  utterances: {
+    repo: "galaxymonster/galaxymonster.github.io",
+    type: "pathname",
+  },
+}
