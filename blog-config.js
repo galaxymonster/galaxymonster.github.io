965e41d51ecc0793b67004d5544d01d1b9a9bebc
caste77an <hancastellan@naver.com>
2023-08-28 15:26:29 +0900

    modify png, home comment

--- blog-config.js
+++ blog-config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  title: "hoodie",
-  description:
-    "Hello :) I'm galaxymonster who developed gatsby-starter-hoodie theme.",
-  author: "Hudi",
+  title: "galaxymonster",
+  description: "Hello :) I'm galaxymonster who loves music.",
+  author: "galaxymonster",
   siteUrl: "https://galaxymonster.github.io/",
   links: {
-    github: "https://github.com/devHudi",
+    github: "https://github.com/galaxymonster",
     linkedIn: "https://linkedin.com",
     facebook: "https://www.facebook.com",
     instagram: "https://www.instagram.com",
-    email: "mailto:hancastellan@gmail.com",
+    email: "mailto:he3074@gmail.com",
   },
   utterances: {
     repo: "galaxymonster/galaxymonster.github.io",
